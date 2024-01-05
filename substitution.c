8c2c6d8c59047372cbde7e449249022505f17f57
xybelle <xybelle@users.noreply.github.com>
2024-01-05 14:03:46 +0800

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -108,14 +108,14 @@ char ciphertext(char plaintext, string key)
         if (isupper(plaintext))
         {
             ci = plaintext - 65;
-            cipher = key[ci];
-            return cipher - 32;
+            cipher = key[ci] % 26;
+            return cipher - 65;
         }
         else if (islower(plaintext))
         {
             ci = plaintext - 97;
-            cipher = key[ci];
-            return cipher + 32;
+            cipher = key[ci] % 26;
+            return cipher + 97;
         }
     }
     return plaintext;
