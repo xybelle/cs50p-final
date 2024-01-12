1f6563b4a94c0caad7556e13f65a474558de5d7e
xybelle <xybelle@users.noreply.github.com>
2024-01-13 00:33:48 +0800

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -105,8 +105,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         {
             float counter = 0.0, red = 0.0, green = 0.0, blue = 0.0;
             // Corners
-            if (((i == 0 && j == 0) || ((i == height - 1) && j == 0) || (i == 0 && (j == width - 1)) ||
-                ((i == height - 1) && (j == width - 1))))
+            if (((i == 0 && j == 0) || ((i == height - 1) && j == 0) || (i == 0 && (j == width - 1))
+                || ((i == height - 1) && (j == width - 1))))
             {
                 for (int x = 0; x < 2; x++)
                 {
@@ -123,8 +123,41 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image[i][j].rgbtBlue = round(blue / (float) counter);
             }
 
-            // Edge
-            //if ( i == 0 && j > 1)
+            // Left and right edges
+            if ( (j == 0 && ((i >= 1) && (i < height - 2))) || (j == width - 1 && ((i >= 1) && (i < height - 2))))
+            {
+                for (int x = -1; x < 2; x++)
+                {
+                    for (int y = 0; y < 2; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
+
+            // Upper and lower edges
+            if ((i == 0 && ((j >= 1) && (j < width - 2))) || (i == height - 1 && ((j >= 1) && (j < width - 2))))
+            {
+                for (int x = 0; x < 2; x++)
+                {
+                    for (int y = -1; y < 2; y++)
+                    {
+                        red += copy[i + x][j + y].rgbtRed;
+                        green += copy[i + x][j + y].rgbtGreen;
+                        blue += copy[i + x][j + y].rgbtBlue;
+                        counter++;
+                    }
+                }
+                image[i][j].rgbtRed = round(red / (float) counter);
+                image[i][j].rgbtGreen = round(green / (float) counter);
+                image[i][j].rgbtBlue = round(blue / (float) counter);
+            }
 
             // Middle part
             if ((i >= 1 && i <= (height - 2)) || (j >= 1 && j <= (width - 2)))
