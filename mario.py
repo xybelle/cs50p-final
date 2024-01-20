dfdda3729fe4cb38a2bf4e9f14b0d6c38c57706f
xybelle <xybelle@users.noreply.github.com>
2024-01-20 22:47:17 +0800

    automated commit by check50 [check50=True]

--- mario.py
+++ mario.py
@@ -4,7 +4,7 @@ from cs50 import get_int
 def main():
     while True:
         height = get_int("Height: ")
-        if height > 0 or height < 8:
+        if height > 0 and height < 8:
             break
     draw(height)
 
