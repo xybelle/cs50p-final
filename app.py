dedffc8dad1f9db693bec74383692e5b5892e3fd
xybelle <xybelle@users.noreply.github.com>
2024-01-31 20:24:00 +0800

    automated commit by check50 [check50=True]

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def buy():
 
         if stock == None:
             return apology("Symbol does not exist")
-        elif not isinstance(shares, int):
+        elif not shares.isdigit():
             return apology("Enter an integer")
         elif shares is None or shares == "" or int(shares) <= 0:
             return apology("Enter the number of shares you wish to buy", 400)
