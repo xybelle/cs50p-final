e9f80a16b6e2f8bd15e79223084ef7e5e61e5454
xybelle <xybelle@users.noreply.github.com>
2024-02-04 23:29:02 +0800

    automated commit by submit50

---
+++ GDS/app.py
@@ -0,0 +1,205 @@
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import login_required
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///gds.db")
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        fname = request.form.get("fname")
+        sname = request.form.get("sname")
+        guardian = request.form.get("parent-guardian")
+        email = request.form.get("email")
+        pw = request.form.get("password")
+        conf = request.form.get("confirmation")
+
+        # Check if email is already registered
+        stu = db.execute("SELECT email FROM students WHERE email = ?", email)
+        existing_stu = stu[0] if stu else None
+
+        # Check if email already exists in the database
+        if existing_stu:
+            return render_template("apology.html", message="Email already registered. Please try logging in instead.")
+        # Validate submission
+        elif not fname or not sname:
+            return render_template("apology.html", message="Please enter your name")
+        # Ensure password is entered
+        elif not pw:
+            return render_template("apology.html", message="Please enter a password")
+        # Ensure password and confirmation matches
+        elif pw != conf:
+            return render_template("apology.html", message="Passwords do not match")
+
+        # Hash password
+        hashed_pw = generate_password_hash(pw, method='pbkdf2', salt_length=2)
+
+        try:
+            # Remember students
+            db.execute("INSERT INTO students (fname, sname, email, hash, guardian) VALUES (?, ?, ?, ?, ?)",
+                       fname, sname, email, hashed_pw, guardian)
+
+        except Exception as e:
+            print(e)
+            return render_template("apology.html", message="Something went wrong. Please try again.")
+
+        rows = db.execute("SELECT * FROM students WHERE email = ?", email)
+        session["user_id"] = rows[0]["id"]
+
+        # Confirm registration
+        return redirect("/home")
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("email"):
+            return render_template("apology.html", message="Please enter your email")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("apology.html", message="Please enter your password")
+
+        # Query database for email
+        rows = db.execute(
+            "SELECT * FROM students WHERE email = ?", request.form.get("email")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return render_template("apology.html", message="Invalid email and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to index
+        return redirect("/home")
+
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to index
+    return redirect("/")
+
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
+
+@app.route("/home")
+@login_required
+def home():
+    # Get name to display greeting
+    name = db.execute("SELECT guardian FROM students WHERE id = ?", session["user_id"])
+    guardian = name[0]
+
+    # Get upcoming classes student already enrolled in
+    enrolled = db.execute("SELECT name FROM enrolments WHERE student_id = ?", session["user_id"])
+    if enrolled != []:
+        enrolled_in = enrolled[0]
+        return render_template("home.html", name = guardian, c=enrolled_in)
+
+    return render_template("home.html", name=guardian, c=enrolled)
+
+
+@app.route("/book", methods=["GET", "POST"])
+@login_required
+def book():
+    if request.method == "POST":
+        selected_class = request.form.get('classes')
+        return render_template("confirm.html", class_name=selected_class)
+    return render_template("book.html")
+
+
+@app.route("/confirm", methods=["GET", "POST"])
+@login_required
+def confirm():
+    if request.method == "POST":
+        selected_class = request.form.get('confirmation')
+
+        # Check if user already enrolled in class
+        already_enrolled = db.execute("SELECT name FROM enrolments WHERE name = ? AND student_id = ?", selected_class, session["user_id"])
+
+        if already_enrolled != []:
+            return render_template("apology.html", message="You've already enrolled in this class")
+
+        # Update enrolments database
+        try:
+            class_id = db.execute("SELECT id FROM classes WHERE name = ?", selected_class)
+            print(class_id)
+            db.execute("INSERT INTO enrolments (class_id, student_id, name) VALUES (?, ?, ?)",
+                       class_id[0]['id'], session["user_id"], selected_class)
+
+        except Exception as e:
+            print(e)
+            return render_template("apology.html", message="Something went wrong. Please try again.")
+    return render_template("booked.html")
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/change_password", methods=["GET", "POST"])
+@login_required
+def change_password():
+    if request.method == "GET":
+        return render_template("change_password.html")
+
+    if request.method == "POST":
+        pw = request.form.get("password")
+        confirm = request.form.get("confirmation")
+        newpw = request.form.get("new_password")
+
+        if not pw or not confirm or not newpw:
+            return render_template("apology.html", message="Please enter password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM students WHERE id = ?", session["user_id"])
+
+        # Check database if old password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], pw):
+            return render_template("apology.html", message="Password incorrect")
+
+        if pw != confirm:
+            return render_template("apology.html", message="Old password and confirmation don't match")
+        else:
+            # Hash new password
+            hashed_new_pw = generate_password_hash(newpw, method='pbkdf2', salt_length=2)
+
+            # Update database with new hashed password
+            db.execute("UPDATE students SET hash = ? WHERE id = ?", hashed_new_pw, session["user_id"])
+
+            return redirect("/")
---
+++ README.md
@@ -0,0 +1,27 @@
+# book it
+#### Video Demo: https://youtu.be/qXxA4oaRb2I
+#### Description:
+My final project for CS50X 2024 online course offered by Harvard University. I've used Flask web framework and some software development/web programming languages covered by the course such as Python, SQLite, HTML, and CSS. 'book it' is a web app for a school that offers primay dance classes to handle or manage enrolments. in 'book it' a parent can register and book classes for their child. Right now, it only assumes one email address per parent-student.
+
+1. **app.py**
+    - Contains functions:
+        - 'index' : Renders index.html
+        - 'register': It requires that user enters the first and last name of student, their name (parent/guardian), email address, password and confirmation. Will present different error message depending on issue. Will return message if name fields are empty, if email is already registered, if password field is empty, if password and confirmation do not match. Passwords are hashed before storing into *students* table in gds.db (more on this on gds.db section). After successful registration, user is logged in.
+        - 'login': Requires that user enter email address and password to log in. Will return error if user did not enter email and/or password. Checks the database for the email address and if password is correct and set session id as user id.
+        - 'logout': Forgets user and redirect user to index.
+        - 'home': Requires user is logged in. Query database to get the parent or guardian name for greeting. Will show any upcoming classes if there are any. Otherwise will show 'no upcoming classes'.
+        - 'book': Renders book.html where user can select which class they want to book. Requires user is logged in.
+        - 'confirm': Gets selected class after user confirms. Will check if user already booked the class by querying the database, if so, will show message 'already enrolled'. Otherwise, will try to update the *enrolments* table except if there are any errors. Requires user is logged in.
+        - 'about': Renders about.html.
+        - 'change_password': Allows user to change their password. Requires that user is logged in. Requires that user enters old password and confirm the old password, then nominate a new password. If a field is empty it will show an error message. Query the database for username (or email) and checks that the password is correct. Will check if old password and confirmation matches, if not, will show an error message. If matches, will proceed to hash the new password and update the hashed password in students table.
+2. **gds.db**
+    - Contains 3 tables:
+        - students: Contains student_id, fname, sname, parent or guardian name, email address and hash (password).
+        - classes: Contains class_id, name of class, teacher's name, semester, year, and status
+        - enrolments - Contains class_id, class name, student_id, and enrolment date.
+3. **helpers.py**
+    - Where login_required decorator is defined to ensure that a user is logged in before accessing specific route.
+
+
+
+
