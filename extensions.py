4a24e401d7aed23ef99dd8d0b06d99ef65cae803
xybelle <xybelle@users.noreply.github.com>
2024-02-06 18:08:22 +0800

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -1,8 +1,12 @@
 f = input("File name: ").lower().lstrip().rstrip().partition('.')
 
 if f[2] == 'gif' or f[2] == 'jpg' or f[2] == 'jpeg' or f[2] == 'png':
+    if f[2] == 'jpg' or f[2] == 'jpeg':
+        print("image/jpeg")
     print(f"image/{f[2]}")
-if f[2] == 'pdf' or f[2] == 'zip':
+elif f[2] == 'pdf' or f[2] == 'zip':
     print(f"application/{f[2]}")
-if f[2] == 'txt':
+elif f[2] == 'txt':
     print(f"txt/{f[2]}")
+else:
+    print("application/octet-stream")
