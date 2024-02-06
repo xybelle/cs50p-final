942eb8ec3881184e3381743b8d9945ced7f27524
xybelle <xybelle@users.noreply.github.com>
2024-02-06 18:41:45 +0800

    automated commit by check50 [check50=True]

--- extensions.py
+++ extensions.py
@@ -5,13 +5,13 @@ if f.count('.') >= 2 or f.count('.') == 0:
 
 f = f.split('.')
 
-if f[len(f)] == 'gif' or f[len(f)] == 'png':
-    print(f"image/{f[len(f)]}")
-elif f[len(f)] == 'jpg' or f[len(f)] == 'jpeg':
+if f[len(f) - 1] == 'gif' or f[len(f) - 1] == 'png':
+    print(f"image/{f[len(f) - 1]}")
+elif f[len(f) - 1] == 'jpg' or f[len(f) - 1] == 'jpeg':
     print("image/jpeg")
-elif f[len(f)] == 'pdf' or f[len(f)] == 'zip':
+elif f[len(f) - 1] == 'pdf' or f[len(f) - 1] == 'zip':
     print(f"application/{f[len(f)]}")
-elif f[len(f)] == 'txt':
+elif f[len(f) - 1] == 'txt':
     print("text/plain")
 else:
     print("application/octet-stream")
