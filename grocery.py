a6bd6a0c8852731e6db9e58b2afd30865f9cdf13
xybelle <xybelle@users.noreply.github.com>
2024-02-10 05:42:28 +0800

    automated commit by check50 [check50=True]

---
+++ grocery.py
@@ -0,0 +1,12 @@
+grocery_list = {}
+while True:
+    try:
+        item = input()
+        if item in grocery_list:
+            grocery_list[item] += 1
+        else:
+            grocery_list[item] = 1
+    except EOFError:
+        for item in grocery_list:
+            print(f"{grocery_list.get(item)} {item.upper()}")
+        break
