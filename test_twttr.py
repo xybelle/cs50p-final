b75e18a95101322b6e0fbb4a27e50fd55e571960
xybelle <xybelle@users.noreply.github.com>
2024-02-14 08:15:30 +0800

    automated commit by submit50

--- test_twttr.py
+++ test_twttr.py
@@ -12,5 +12,6 @@ def test_uppercase():
 def test_numbers():
     assert shorten("1 day Two DAys 3 days") == "1 dy Tw Dys 3 dys"
 
+
 def test_punc():
     assert shorten("Can you hear me?") == "Cn y hr m?"
