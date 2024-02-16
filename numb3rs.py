3fc48f0ed8c55e10b45ec22f4d9af34e29e1d51e
xybelle <xybelle@users.noreply.github.com>
2024-02-16 09:22:12 +0800

    automated commit by check50 [check50=True]

--- numb3rs.py
+++ numb3rs.py
@@ -7,22 +7,26 @@ def main():
 
 def validate(ip):
     if valid := re.search(r"([0-2]?[0-9]?[0-9]?\.){3}[0-2]?[0-9]?[0-9]?", ip):
-        if max_255(ip):
+        if max_255(ip) and four_sets(ip):
             return True
     return False
 
 
 def max_255(digits):
-    parts = digits.split(".")
-    if len(parts) > 4:
+    digits = digits.split(".")
+    for digit in digits:
+        if not digit.isdigit():
+            return False
+        if int(digit) > 255:
+            return False
+            break
+    return True
+
+
+def four_sets(n):
+    n = n.split(".")
+    if len(n) > 4:
         return False
-    for part in parts:
-        if not part.isdigit():
-            return False
-            break
-        if int(part) > 255:
-            return False
-            break
     return True
 
 
