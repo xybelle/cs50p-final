04ac5fc54c72eeaddcc86dddff4abba15fd68955
xybelle <xybelle@users.noreply.github.com>
2024-02-16 09:41:25 +0800

    automated commit by check50 [check50=True]

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -1,18 +1,19 @@
-from numb3rs import validate, max_255, four_sets
+import numb3rs
+
 
 def test_validate():
-    assert validate("1.2.3.4") == True
-    assert validate("255.255.255.255") == True
-    assert validate("275.1.1.1") == False
-    assert validate("cat") == False
+    assert numb3rs.validate("1.2.3.4") == True
+    assert numb3rs.validate("255.255.255.255") == True
+    assert numb3rs.validate("275.1.1.1") == False
+    assert numb3rs.validate("cat") == False
 
 
-def test_validate_max_255():
-    assert validate("256.255.255.255") == False
-    assert validate("255.255.255.255") == True
-    assert validate("1.1.1.1") == True
+def test_max_255():
+    assert numb3rs.max_255("256.255.255.255") == False
+    assert numb3rs.max_255("255.255.255.255") == True
+    assert numb3rs.max_255("1.1.1.1") == True
 
 
-def test_validate_four_sets():
-    assert four_sets("1.1.1.1.1") == False
-    assert four_sets("1.1.1.1") == True
+def test_four_sets():
+    assert numb3rs.four_sets("1.1.1.1.1") == False
+    assert numb3rs.four_sets("1.1.1.1") == True
