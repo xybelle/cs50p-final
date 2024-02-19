b02ffbe17fb49f12aba516649054bee8103fc2dd
xybelle <xybelle@users.noreply.github.com>
2024-02-19 11:12:25 +0800

    automated commit by check50 [check50=True]

--- working.py
+++ working.py
@@ -8,52 +8,53 @@ def main():
 
 def convert(s):
     valid_time = validate_input(s)
-    if valid_time[1] == "PM":
-        h1, m1 = for_pm(valid_time[0])
-    elif valid_time[1] == "AM":
-        h1, m1 = for_am(valid_time[0])
-    if valid_time[3] == "PM":
-        h2, m2 = for_pm(valid_time[2])
-    elif valid_time[3] == "AM":
-        h2, m2 = for_am(valid_time[2])
+    #print(valid_time)
+    #print(valid_time[0])
+    if re.search(r"(?:1[0-2]|[1-9]):[0-5][0-9]", valid_time[0]):
+        h, m1 = valid_time[0].split(":")
+        h1 = hours_mins_fmt(h, valid_time[1])
+    elif re.search(r"(?:1[0-2]|[1-9])", valid_time[0]):
+        h1 = hours_fmt(valid_time[0], valid_time[1])
+        m1 = 0
+    if re.search(r"(?:1[0-2]|[1-9]):[0-5][0-9]", valid_time[2]):
+        hh, m2 = valid_time[2].split(":")
+        h2 = hours_mins_fmt(hh, valid_time[3])
+    elif re.search(r"(?:1[0-2]|[1-9])", valid_time[2]):
+        h2 = hours_fmt(valid_time[2], valid_time[3])
+        m2 = 0
+    #print(f"{h1:02}:{m1:02} to {h2:02}:{m2:02}")
     return f"{h1:02}:{m1:02} to {h2:02}:{m2:02}"
 
 
 def validate_input(s):
-    try:
-        if matches := re.search(r"((?:1[0-2]|[1-9]):?[0-5]?[0-9]?) (AM|PM) to ((?:1[0-2]|[1-9]):?[0-5]?[0-9]?) (AM|PM)", s):
+    #try:
+        if matches := re.search(r"((?:1[0-2]|[1-9]):?(?:[0-5][0-9])?) (AM|PM) to ((?:1[0-2]|[1-9]):?(?:[0-5][0-9])?) (AM|PM)", s):
             return matches.groups()
         else:
             raise ValueError
-    except ValueError:
-        sys.exit("Hours: time AM/PM to time AM/PM")
+    #except ValueError:
+        #print("Hour: time AM/PM to time AM/PM")
 
 
-def for_pm(time):
-    if ":" in time:
-            h, m = time.split(":")
-            h = int(h) + 12
-            return h, int(m)
-    else:
-        h = int(time) + 12
-        m = 0
-        return h, m
+def hours_mins_fmt(hour, mid):
+    if mid == "AM":
+        if  hour == "12":
+            hour = 0
+        return int(hour)
+    if mid == "PM":
+        hour = int(hour) + 12
+        return int(hour)
 
 
-def for_am(time):
-    if ":" in time:
-        h, m = time.split(":")
-        if h == "12":
-            h = 0
-        return int(h), int(m)
-    else:
-        if time == "12":
-            h = 0
-        h = int(time)
-        m = 0
-        return h, m
+def hours_fmt(hour, mid):
+    if mid == "AM":
+        if hour == "12":
+            hour = 0
+        return int(hour)
+    if mid == "PM":
+        hour = int(hour) + 12
+        return int(hour)
 
 
 if __name__ == "__main__":
     main()
-
